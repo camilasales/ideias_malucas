45ce3ece111a281dd234559f927e88ea688cebae
Camila Sales <camila.07ramones@gmail.com>
2019-07-29 16:41:44 -0300

    deletar logicamente

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
-from website.views import index, sobre, login, cadastrar_ideia
+# aqui deve ser importado os meodos da viws
+# é uma má pratica importar um de cada vez
+from website.views import index, sobre, login, cadastrar_ideia, remover_ideia
 
 urlpatterns = [
     path('', index),
     path('sobre', sobre),
     path('login', login),
-    path('ideias', cadastrar_ideia)
+    path('ideias', cadastrar_ideia),
+    path('remover_ideia/<int:id>', remover_ideia)
 ]
